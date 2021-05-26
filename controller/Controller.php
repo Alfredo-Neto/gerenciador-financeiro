93a4611d9ddf1d3eaa4ee317d92f6b6969b1cd98
Alfredo Braule <alfredoneto934@gmail.com>
2021-05-26 20:02:30 -0300

    criamos um form e botoes estilizados e dinamicos

--- controller/Controller.php
+++ controller/Controller.php
@@ -3,7 +3,7 @@
 class Controller
 {
     private $delimitador = '9416485941';
-    private $qtdTempoToken = 1;
+    private $qtdTempoToken = 60 * 10 * 6;
 
     protected function makeAWT($nome,$id){
         $data = new Datetime();
@@ -27,18 +27,18 @@ class Controller
         $dataAtual = new Datetime();
         $dataToken = new Datetime($arrayDados[3]);
 
-
         $resultado = $dataToken->getTimestamp() - $dataAtual->getTimestamp();
         if($resultado <= 0){
             throw new AuthorizationException("Seu token não é mais valido! Favor Relogar!", 1);
         }
 
         $pdo = DbConnectionFactory::get();
-        $sql = "SELECT * FROM Usuarios where name like '$arrayDados[2]'";
+        $sql = "SELECT * FROM Usuarios where id = $arrayDados[2] and name like '$arrayDados[1]'";
+
         $statement = $pdo->prepare($sql);
         $statement->execute();
         $usuariosEncontrados = $statement->fetch(PDO::FETCH_ASSOC);
-        
+
         if(!$usuariosEncontrados) {
             throw new AuthorizationException("Usuário inválido! Favor Relogar!", 1);
         }
