470b0bb8523c49d74b7ea32896799a13ba8afad1
Alfredo Braule <alfredoneto934@gmail.com>
2021-05-30 20:05:13 -0300

    created verification that evaluates if a user can/cannot see a list of accounts, by binding foreign key usuario_id to sql SELECT statement and avoiding users to see accounts from other users

--- controller/ContasController.php
+++ controller/ContasController.php
@@ -7,13 +7,14 @@ class ContasController extends Controller
         try {
             if(!property_exists($request, 'token_awt') || $request->token_awt == null 
             || $request->token_awt == ''){
-                throw new Exception("Please inform token_awt field.", 1);
+                throw new AuthorizationException ("Please inform token_awt field.", 1);
             }
-            $this->validateAWT($request->token_awt);
-            
+            $arrDados = $this->validateAWT($request->token_awt);
+
             $pdo = DbConnectionFactory::get();
-            $sql = "SELECT * FROM Contas";
+            $sql = "SELECT * FROM Contas WHERE usuario_id = :usuario_id";
             $statement = $pdo->prepare($sql);
+            $statement->bindValue(':usuario_id', $arrDados[2]);
             $statement->execute();
             $contasEncontradas = $statement->fetchAll(PDO::FETCH_ASSOC);
             return new JsonResponse (['contas' => $contasEncontradas], 200);
@@ -34,7 +35,13 @@ class ContasController extends Controller
     public function create($request)
     {
         try {
-
+            if(!property_exists($request, 'token_awt') || $request->token_awt == null
+            || $request->token_awt == ''){
+                throw new Exception("Please inform token_awt field.", 1);
+            }
+            
+            $arrDados = $this->validateAWT($request->token_awt);
+            
             if(!property_exists($request, 'nome') || $request->nome == null 
             || $request->nome == ''){
                 throw new Exception("Please inform nome field.", 1);
@@ -45,13 +52,6 @@ class ContasController extends Controller
                 throw new Exception("Please inform saldo field.", 1);
             }
             
-            if(!property_exists($request, 'token_awt') || $request->token_awt == null 
-            || $request->token_awt == ''){
-                throw new Exception("Please inform token_awt field.", 1);
-            }
-
-            $arrDados = $this->validateAWT($request->token_awt);
-
             $pdo = DbConnectionFactory::get();
             $sql = "INSERT INTO Contas (nome, saldo, usuario_id)
             VALUES (:nome, :saldo, :usuario_id)";
